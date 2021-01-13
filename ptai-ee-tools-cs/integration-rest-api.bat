f4c302e3d3b52c8945dd20497668fbab7b036ae9
Alexey N. Zhukov <alexey.n.zhukov@gmail.com>
2021-01-13 07:18:29 +0300

    Redundant modules are removed

--- ptai-ee-tools-cs/integration-rest-api.bat
+++
@@ -1,1 +0,0 @@
-java -jar D:\TOOLS\DEVEL\MISC\OPENAPI\openapi-generator-cli.jar generate -g csharp -i ..\ptai-ee-tools-java\rest-api\integration-rest-api\src\main\resources\api\integration.yml --package-name AI.Enterprise.Integration.RestApi -o .\integration-rest-api
