5f839f52b2ab5d0c519d3df3d7589e572151769f
Andrey Lyadusov <alyadusov@ptsecurity.com>
2023-06-29 02:16:08 +0300

    Add 4.5.0 api integration tests

--- ptai-data-structures/src/main/java/com/ptsecurity/appsec/ai/ee/scan/result/ScanBrief.java
+++ ptai-data-structures/src/main/java/com/ptsecurity/appsec/ai/ee/scan/result/ScanBrief.java
@@ -25,7 +25,7 @@ public class ScanBrief {
         @Deprecated V36,
         @Deprecated V40,
         @Deprecated V41,
-        V411, V420, V430, V44X;
+        V411, V420, V430, V44X, V450;
 
         @SneakyThrows
         public static boolean isDeprecated(@NonNull final ApiVersion version) {
