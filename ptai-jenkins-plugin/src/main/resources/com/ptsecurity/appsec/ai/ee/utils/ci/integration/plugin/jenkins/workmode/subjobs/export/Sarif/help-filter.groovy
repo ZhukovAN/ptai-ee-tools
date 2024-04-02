fd7f970a7f7224da6f1d0a5d2a85832a1d49e716
dbakin <dbakin@ptsecurity.com>
2024-04-02 11:40:58 +0300

    Fix vulnerability filter hints

---
+++ ptai-jenkins-plugin/src/main/resources/com/ptsecurity/appsec/ai/ee/utils/ci/integration/plugin/jenkins/workmode/subjobs/export/Sarif/help-filter.groovy
@@ -0,0 +1,5 @@
+package com.ptsecurity.appsec.ai.ee.utils.ci.integration.plugin.jenkins.workmode.subjobs.export.Sarif
+
+import com.ptsecurity.appsec.ai.ee.utils.ci.integration.Resources
+
+text(Resources.i18n_ast_settings_mode_synchronous_subjob_export_sarif_filter_hint())
